36d07192195606fc1d82851097eb98648bab246a
amitthakur2013 <amitthakur2013@gmail.com>
2021-03-03 21:55:31 +0530

    java anno

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,27 +1,23 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
 public class SwimCoach implements Coach {
 	
-	@Value("${foo.email}")
-	private String email;
+	private FortuneService fortuneService;
 	
-	@Value("${foo.team}")
-	private String team;
-	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
-		return "From Swim Coach";
+		
+		return "Swim 1000 metre!! as a warm up!!";
 	}
 
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return this.email;
+		return fortuneService.getFortune();
 	}
 
 }
