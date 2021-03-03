bedd8cff37a02e4b9557e5ea10190eb70eab0af1
amitthakur2013 <amitthakur2013@gmail.com>
2021-03-03 13:59:55 +0530

    Scope prototype

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component //(write the id for the bean)
+@Scope("prototype") //singleton | prototype
 public class TennisCoach implements Coach {
 	
 	
