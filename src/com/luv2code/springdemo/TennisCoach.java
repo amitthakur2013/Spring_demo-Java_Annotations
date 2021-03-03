d30f80a6180ab4589f8f18764bcde33eb6077c34
amitthakur2013 <amitthakur2013@gmail.com>
2021-03-03 12:31:22 +0530

    qualifier annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component //(write the id for the bean)
@@ -9,6 +10,7 @@ public class TennisCoach implements Coach {
 	
 	//Field injection direct injection also to private one
 	@Autowired
+	@Qualifier("happyFortuneService") //for multiple beans specify the bean id
 	private FortuneService fortuneService;
 	
 	/*Costructor Injection*/
